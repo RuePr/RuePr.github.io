ec9582821ca102c29b9e19790ff14e95d85fe328
RuePr <103666829+RuePr@users.noreply.github.com>
2022-04-14 13:54:08 -0400

    Update save_data.php

--- save_data.php
+++ save_data.php
@@ -1,7 +1,8 @@
 <?php
 // the $_POST[] array will contain the passed in filename and data
 // the directory "data" is writable by the server (chmod 777)
-$filename = "data/".$_POST['filename'];
+//https://github.com/RuePr/RuePr.github.io/tree/main/data
+$filename = "main/data/".$_POST['filename'];
 $data = $_POST['filedata'];
 // write the file to disk
 file_put_contents($filename, $data);
